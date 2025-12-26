0e6ebc0397041c65b27d0befc11897b155d3f82d
NAGA VENKATA RAVITEJA UNGARALA <unvraviteja@gmail.com>
2025-12-26 06:56:54 +0000

    make background white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>
         <Header />
         <main className="flex-1">{children}</main>
